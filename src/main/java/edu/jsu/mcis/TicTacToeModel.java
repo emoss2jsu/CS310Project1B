89bd85883f7998b8b10e0ff851d0c2665b835aae
Earl <emoss2@stu.jsu.edu>
2017-09-25 18:28:14 -0500

    Initial Commit Passing first two test

---
+++ src/main/java/edu/jsu/mcis/TicTacToeModel.java
@@ -0,0 +1,260 @@
+package edu.jsu.mcis;
+
+public class TicTacToeModel{
+    
+    private static final int DEFAULT_WIDTH = 3;
+    
+    /* Mark (represents X, O, or an empty square */
+    
+    public enum Mark {
+        
+        X("X"), 
+        O("O"), 
+        EMPTY("-");
+
+        private String message;
+        
+        private Mark(String msg) {
+            message = msg;
+        }
+        
+        @Override
+        public String toString() {
+            return message;
+        }
+        
+    };
+    
+    /* Result (represents the final state of the game: X wins, O wins, a tie,
+       or NONE if the game is not yet over) */
+    
+    public enum Result {
+        
+        X("X"), 
+        O("O"), 
+        TIE("Tie"), 
+        NONE("none");
+
+        private String message;
+        
+        private Result(String msg) {
+            message = msg;
+        }
+        
+        @Override
+        public String toString() {
+            return message;
+        }
+        
+    };
+    
+    private Mark[][] grid; /* Game grid */
+    private boolean xTurn; /* True if X is current player */
+    private int width;     /* Size of game grid */
+    
+    /* DEFAULT CONSTRUCTOR */
+    
+    public TicTacToeModel() {
+        
+        /* No arguments (call main constructor; use default size) */
+        
+        this(DEFAULT_WIDTH);
+        
+    }
+    
+    /* CONSTRUCTOR */
+    
+    public TicTacToeModel(int width) {
+        
+        /* Initialize width; X goes first */
+        
+        this.width = width;
+        xTurn = true;
+        
+        /* Create grid (width x width) as a 2D Mark array */ 
+		
+        /* INSERT YOUR CODE HERE */
+		
+		grid = new Mark[width][width];
+			
+        /* Initialize grid by filling every square with empty marks */
+
+        /* INSERT YOUR CODE HERE */
+        for(int i = 0; i < width; i++){
+			for(int j = 0; j < width; j++){
+				grid[i][j] = Mark.EMPTY;
+			}
+		}
+    }
+	
+    public boolean makeMark(int row, int col) {
+        
+        /* Place the current player's mark in the square at the specified
+           location, but only if the location is valid and if the square is
+           empty! */
+        
+        /* INSERT YOUR CODE HERE */	
+		if((isValidSquare(row,col) == true) && (isSquareMarked(row,col)== false)){
+			if(xTurn){
+				grid[row][col] = Mark.X;
+				xTurn = false;
+				return true;
+			}
+			else{
+				grid[row][col] = Mark.O;
+				xTurn = true;
+				return true;
+			}
+	    }
+		else return false;
+		
+        
+    }
+	
+    private boolean isValidSquare(int row, int col) {
+        
+        /* Return true if specified location is within grid bounds */
+        
+        /* INSERT YOUR CODE HERE */
+		if(row >= width || row < 0 || col >= width || col < 0){
+			return false;
+		}
+		else return true;
+		
+    }
+	
+    private boolean isSquareMarked(int row, int col) {
+        
+        /* Return true if square at specified location is marked */
+        
+        /* INSERT YOUR CODE HERE */
+		if(grid[row][col].equals(Mark.EMPTY)){
+			return false;
+		}
+		else return true;
+    }
+	
+    public Mark getMark(int row, int col) {
+        
+        /* Return mark from the square at the specified location */
+        
+        /* INSERT YOUR CODE HERE */
+		return grid[row][col];		
+    }
+	
+    public Result getResult() {
+        
+        /* Use isMarkWin() to see if X or O is the winner, if the game is a
+           tie, or if the game is not over, and return the corresponding Result
+           value */
+        
+        /* INSERT YOUR CODE HERE */
+		if(isMarkWin(Mark.X)){
+			return Result.X;
+		}
+		else if(isMarkWin(Mark.O)){
+			return Result.O;
+		}
+		else if(isTie()){
+			return Result.TIE;
+		}
+		else return Result.NONE;
+		
+    }
+	
+    private boolean isMarkWin(Mark mark) {
+        
+        /* Check the squares of the board to see if the specified mark is the
+           winner */
+        
+        /* INSERT YOUR CODE HERE */
+		
+		boolean won = true;
+		
+		for(int i = 0; i < width; i++){
+			won = true;
+			for(int j = 0; j < width; j++){
+				if(!grid[j][i].equals(mark)){
+					won = false;
+				}
+			}
+			if(won){return true;}
+		}
+		
+		
+		
+		
+		for(int i = 0; i < width; i++){
+			won = true;
+			for(int j = 0; j < width; j++){
+				if(!grid[i][j].equals(mark)){
+					won = false;
+				}
+			}
+			if(won){return true;}
+		}
+		
+		
+		won = true;
+		
+		for(int i = 0; i < width; i++){
+			if(!grid[i][i].equals(mark)){
+				won = false;
+			}
+		}
+		if(won){
+			return true;
+			}
+		won = true;
+		for(int i = 0; i < width; i++){	
+			if(!grid[i][width-1-i].equals(mark)){
+				won = false;
+			}
+		}
+		if(won){
+			return true;
+		}	
+		return false;
+		
+    }
+	
+    private boolean isTie() {
+        
+        /* Check the squares of the board to see if the game is a tie */
+		
+        /* INSERT YOUR CODE HERE */
+		for(int i = 0; i < getWidth(); i++){
+			for(int j = 0; j < getWidth(); j++){
+				if((getMark(i,j).equals(Mark.EMPTY))){
+					return false;
+				}
+			}
+		}
+		return true;
+    }
+
+    public boolean isGameover(){
+        
+        /* Return true if the game is over */
+        
+        return Result.NONE != getResult();
+        
+    }
+
+    public boolean isXTurn(){
+        
+        /* Getter for xTurn */
+        
+        return xTurn;
+        
+    }
+
+    public int getWidth(){
+        
+        /* Getter for width */
+        
+        return width;
+        
+    }
+    
+}
---
+++ src/main/java/edu/jsu/mcis/TicTacToeView.java
@@ -0,0 +1,127 @@
+package edu.jsu.mcis;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class TicTacToeView extends JPanel implements ActionListener {
+    
+    TicTacToeModel model;
+
+    private JButton[][] squares;
+    private JPanel squaresPanel;
+    private JLabel resultLabel;
+
+    public TicTacToeView(TicTacToeModel model) {
+
+        this.model = model;
+
+        int width = model.getWidth();
+
+        setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+        squares = new JButton[width][width];
+        squaresPanel = new JPanel(new GridLayout(width,width));
+        resultLabel = new JLabel();
+        resultLabel.setName("ResultLabel");
+        
+        /* Loop through all rows and columns of the grid and: create a new
+           JButton and assign it to the current grid element, add "this"
+           (the View object) as the ActionListener, set the JButton's name
+           to "SquareXY" (where X is the row and Y is the column), set the
+           preferred size to 64x64 pixels, and add the new square to the
+           "squaresPanel" JPanel created earlier. */
+        
+        /* INSERT CODE HERE */
+	
+	for(int i = 0; i < model.getWidth(); i++){
+			for(int j = 0; j < model.getWidth(); j++){
+				squares[i][j] = new JButton();
+				squares[i][j].setName("Square"+ i + j);
+				squares[i][j].addActionListener(this);
+				squares[i][j].setPreferredSize(new Dimension(64, 64));
+				squaresPanel.add(squares[i][j]);
+			}
+		}
+        
+        add(squaresPanel);
+        add(resultLabel);
+        
+        resultLabel.setText("Welcome to Tic-Tac-Toe!");
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent event) {
+        
+        String name = ((JButton) event.getSource()).getName();
+        
+        /* Extract the ROW and COL from the string name (remember, the squares
+           should all be named "SquareXY", so the ROW should be extracted from
+           position 6 of this string, and the COL from position 7.  Remember to
+           convert both to Integers!) */
+
+        /* INSERT CODE HERE */
+	int row = Character.getNumericValue(name.charAt(6));
+	int col = Character.getNumericValue(name.charAt(7));
+           
+        /* Call makeMark() to place the mark in the Model */
+        
+        /* INSERT CODE HERE */
+	model.makeMark(row, col);
+        /* Update the squares of the View using the "updateSquares()" method
+           (see below); this is the equivalent of re-printing the grid to the
+           console in Part One. */
+
+        updateSquares();
+
+        /* Clear the "showResult" label at the bottom of the View */
+        
+        showResult(" ");
+        
+        /* If the game is over (that is, if the "getResult()" method returns
+           a result other than Result.NONE), loop through all the JButtons in
+           the View and disable them to prevent further entries.  (Hint: the
+           JButton class provides a "setEnabled()" method for this.) */
+           
+        /* INSERT CODE HERE */
+		if(model.isXTurn()){
+			resultLabel.setText("X Turn");
+		}
+		else {
+			resultLabel.setText("O Turn");
+		}
+	
+	if(!model.getResult().equals(TicTacToeModel.Result.NONE )){
+			resultLabel.setText(""+model.getResult());
+			for(int i = 0; i < model.getWidth(); i++){
+				for(int j = 0; j < model.getWidth(); j++){
+					squares[i][j].setEnabled(false);
+				}
+			}
+			if(model.getResult().equals(TicTacToeModel.Result.TIE)){
+		resultLabel.setText("Tie");
+	}
+		}
+        
+    }
+        
+    public void updateSquares() {
+
+        /* Loop through all of the rows and columns in the JButton grid and
+           reset the button text to match the corresponding marks as returned
+           by "getMark()" (to update the View to match the Model) */
+           
+        /* INSERT CODE HERE */
+	for(int i = 0; i < model.getWidth(); i++){
+			for(int j = 0; j < model.getWidth(); j++){
+				squares[i][j].setText("" +model.getMark(i,j));
+			}
+		}
+
+    }
+        
+    public void showResult(String message) {
+        resultLabel.setText(message);
+    }
+
+}
